29b9bff945d38620920ff8cb56120e2e4acf533d
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-08-19 16:11:12 +0200

    :recycle: General refactoring

--- src/main/kotlin/xyz/poeschl/challengeplayground/Application.kt
+++ src/main/kotlin/xyz/poeschl/challengeplayground/Application.kt
@@ -2,9 +2,10 @@ package xyz.poeschl.challengeplayground
 
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.default
+import com.xenomachina.argparser.mainBody
 import kotlin.concurrent.thread
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>): Unit = mainBody {
     ArgParser(args).parseInto(::Args).run {
         println("Start drawing on $host:$port")
         val drawer = PlaygroundDrawer(host, port)
@@ -23,7 +24,9 @@ fun main(args: Array<String>) {
             restInterface.start()
         }
         thread {
-            cliInterface.start()
+            if (enableCLI) {
+                cliInterface.start()
+            }
         }
     }
 }
@@ -31,5 +34,6 @@ fun main(args: Array<String>) {
 class Args(parser: ArgParser) {
     val host by parser.storing("--host", help = "The host of the pixelflut server").default("localhost")
     val port by parser.storing("-p", "--port", help = "The port of the pixelflut server") { toInt() }.default(1234)
+    val enableCLI by parser.flagging("--enableCli", help = "Enables the command-line cli").default(false)
     val controlPort by parser.storing("-c", "--controlport", help = "The port for commands") { toInt() }.default(4321)
 }
